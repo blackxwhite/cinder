86780eb385d8957f91e03484410941f13c669ac5
Blake <blackxwhite@gmail.com>
2013-12-23 14:05:30 +0800

    Fix the nfs export rule setting for new version of Netapp API.

--- cinder/volume/drivers/netapp/nfsvolume.py
+++ cinder/volume/drivers/netapp/nfsvolume.py
@@ -240,17 +240,18 @@ class NetAppDirectNfsVolumeDriver(driver.VolumeDriver):
         nfs_export.add_child_elem(rule)
         rule_info = NaElement.create_node_with_children(
             'exports-rule-info',
-            **{'pathname': '/vol/%s' % (name)})
+            **{'pathname': '/vol/%s' % (name),
+               'anon': '0'})
         rw = NaElement('read-write')
-        root = NaElement('root')
+        #root = NaElement('root')
         rw.add_node_with_children(
                 'exports-hostname-info',
-                **{'name': 'all-hosts'})
-        root.add_node_with_children(
-                'exports-hostname-info',
-                **{'name': 'all-hosts'})
+                **{'all-hosts': 'true'})
+        #root.add_node_with_children(
+        #        'exports-hostname-info',
+        #        **{'all-hosts': 'true'})
         rule_info.add_child_elem(rw)
-        rule_info.add_child_elem(root)
+        #rule_info.add_child_elem(root)
         rule.add_child_elem(rule_info)
         self._invoke_successfully(nfs_export, True)
         LOG.debug(_("Export nfs volume /vol/%s") % name)
